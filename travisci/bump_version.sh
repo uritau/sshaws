69728fe4c5786d41c39dae85eb8a416b6a0a0b0f
Oriol Tauleria <oriol.tauleria@gmail.com>
2019-07-15 18:21:37 +0200

    Add autobump

---
+++ travisci/bump_version.sh
@@ -0,0 +1,17 @@
+# !/usr/bin/env bash
+
+version_file="version.go"
+version=`grep "version=" $version_file | cut -d'=' -f 2`
+old_version=${version//\"}
+new_version=`echo $old_version | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g'`
+
+echo "New version is $new_version"
+
+sed -i "s/version=\"$old_version\"/version=\"$new_version\"/g" $version_file
+git add .
+git commit -m "Bump version"
+git tag $NEW_TAG &> /dev/null
+git push origin --tags &> /dev/null
+
+
+
