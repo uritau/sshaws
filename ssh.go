cbed81054b9db29f66a58615a89674a7ef115c2b
Oriol Tauleria <oriol.tauleria@gmail.com>
2019-07-17 22:53:19 +0200

    clean & DX

--- ssh.go
+++ ssh.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 func launch_ssh(dest_ip string) {
 	pa := os.ProcAttr{
 		Files: []*os.File{os.Stdin, os.Stdout, os.Stderr},
 	}
+	time.Sleep(1 * time.Second)
 
 	fmt.Printf(">> Starting a new ssh session to %s\n", dest_ip)
 	proc, err := os.StartProcess("/usr/bin/ssh", []string{"ssh", dest_ip}, &pa)
