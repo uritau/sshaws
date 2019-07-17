a0793db2bda521149838f7cd632adf011c575b36
Oriol Tauleria <oriol.tauleria@gmail.com>
2019-07-17 22:53:19 +0200

    clean & DX

--- select_instance.go
+++ select_instance.go
@@ -7,7 +7,7 @@ import (
 
 func select_instance_index(instance_list []instance) int {
 	var input string
-	fmt.Println("Which one do you want to shh in?")
+	fmt.Println("\nWhich one do you want to shh in?")
 	fmt.Scanln(&input)
 	index, err := strconv.Atoi(input)
 	if err != nil || index > len(instance_list)-1 || index < 0 {
